08ba791cc56ccf48da4adbcf6b3a8fc04d0c9082
OsirisLambert <35780056+OsirisLambert@users.noreply.github.com>
2021-12-01 17:36:37 -0800

    Update load_emotion.py

--- load_emotion.py
+++ load_emotion.py
@@ -25,6 +25,34 @@ def load_emotion(ec_model, nnc_model, dataset, compare_col_name='sentence2'):
 		ct += 1
 	return score/ct
 
+
+# This function can calculate dialog level performance.
+def check_dialog_level_score(compare_column_name):
+    max_total = []
+    min_total = []
+    conver = []
+    for idx, row in df.iterrows():
+        try:
+            label_nn = nnc(row[compare_column_name])[0]['label']
+            if label_nn == 'Neutral':
+                score = 0.5
+            else:
+                label_user = ec(row['user'])[0]['label']# row['sentence1'] ==> user response
+                label_sys = ec(row[compare_column_name])[0]['label']
+                # if user and system are both pos/neg => get 1.0 point
+                if label_user == label_sys:
+                    score = 1
+                else:
+                    score = 0
+            conver.append(score)
+        except:
+            max_total.append(max(conver))
+            min_total.append(min(conver))
+            conver = []
+            continue
+    return sum(max_total) / len(max_total), sum(min_total) / len(min_total)
+
+
 if __name__ == '__main__':
 	dataset = pd.read_csv(csvFile, header=0)
 	ec = pipeline('text-classification', model='Osiris/emotion_classifier')
